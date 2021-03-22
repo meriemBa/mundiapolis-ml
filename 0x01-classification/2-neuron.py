6ef7e0c33755330fa67328d46852a7373f97689e
meriemBa <76498829+meriemBa@users.noreply.github.com>
2021-03-22 10:10:15 +0100

    Create 2-neuron.py

---
+++ 0x01-classification/2-neuron.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+class Neuron:
+    
+
+    def __init__(self, nx):
+       
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    def forward_prop(self, X):
+        
+        preactivation = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-preactivation))
+        return self.__A
+
+    @property
+    def W(self):
+       
+        return self.__W
+
+    @property
+    def b(self):
+     
+        return self.__b
+
+    @property
+    def A(self):
+       
+        return self.__A
