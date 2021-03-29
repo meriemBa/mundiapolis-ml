a5d38affe259c220a96af79b9fe00178ea8485c9
meriemBa <76498829+meriemBa@users.noreply.github.com>
2021-03-29 14:52:42 +0100

    Update 4-neuron.py

--- 0x01-classification/4-neuron.py
+++ 0x01-classification/4-neuron.py
@@ -1,12 +1,9 @@
+#!/usr/bin/env python3
 import numpy as np
 
-
 class Neuron:
-   
-
     def __init__(self, nx):
-       
-
+     
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
